c0348522820870c4a326a4055ecf140317453610
Enomoto Fumiko <enomoto.sy@gmail.com>
2017-02-09 17:08:57 +0900

    投稿機能

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,4 +4,17 @@ def index
 @tweets= Tweet.all
 end
 
+def new
+
+end
+
+def create
+  Tweet.create(name: params[:name], image: params[:image],text: params[:text])
+end
+
+private
+def test_params
+  params.permit(:name,:image,:text)
+end
+
 end
